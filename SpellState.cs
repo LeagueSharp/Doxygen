f94b7c62f1726abb75e9f70562cf613c9c0d6525
Esk0r <kortatu1512@hotmail.com>
2015-02-23 00:47:52 +0100

    Added some redundant documentation for testing purposes

--- SpellState.cs
+++ SpellState.cs
@@ -6,11 +6,34 @@
   /// </summary>
   public enum SpellState
   {
+    /// <summary>
+    /// Ready to be used.
+    /// </summary>
     Ready = 0,
+
+    /// <summary>
+    /// Not level up yet.
+    /// </summary>
     NotLearned = 2,
+
+    /// <summary>
+    /// Spell surpressed
+    /// </summary>
     Surpressed = 3,
+
+    /// <summary>
+    /// On Cooldown
+    /// </summary>
     Cooldown = 4,
+
+    /// <summary>
+    /// Not enough mana to cast the spell.
+    /// </summary>
     NoMana = 5,
+
+    /// <summary>
+    /// Unknown
+    /// </summary>
     Unknown = 10,
   }
 }
